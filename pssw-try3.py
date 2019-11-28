753b8ac8be0b98c49bc88664ff46110c558daee5
Apollo Chiu <eldorado.tw@gmail.com>
2019-11-28 17:34:36 +0800

    fix while loop and print no chance to try

--- pssw-try3.py
+++ pssw-try3.py
@@ -1,11 +1,14 @@
 key = 'a123456'
 x = 3
 while x > 0:
+	x = x - 1
 	psw = input('請輸入密碼: ')
 	if psw == key:
 		print('登入成功!')
 		break
 	else:
-		x = x - 1
+		print('密碼錯誤!')
 		if x > 0:
-			print('密碼錯誤! 還有', x, '次機會')
+			print('還有', x, '次機會')
+		else:
+			print('沒機會嘗試了! 要鎖帳號了啦!')
